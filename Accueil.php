bae3c4548ff2db9114e0ecb26808dab634afad3c
TeooSwee <teoathle@gmail.com>
2026-02-19 13:40:33 +0100

    Debug : affichage des erreurs PHP dans Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require 'db.php';
 $avis = $pdo->query("SELECT a.note, a.description, u.prenom, u.nom FROM avis a JOIN utilisateur u ON u.utilisateur_id = a.utilisateur_id WHERE a.statut = 'validé' ORDER BY a.avis_id DESC LIMIT 6")->fetchAll(PDO::FETCH_ASSOC);
 ?>
