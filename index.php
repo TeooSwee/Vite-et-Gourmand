29ca44181e1a2ea1c4d63ba1f7177c01d6bdc41d
TeooSwee <teoathle@gmail.com>
2026-02-19 11:59:56 +0100

    Debug : affichage des erreurs PHP dans index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// Affiche les erreurs PHP pour le debug
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Redirige vers la page d'accueil principale
 header('Location: Accueil.php');
 exit;
