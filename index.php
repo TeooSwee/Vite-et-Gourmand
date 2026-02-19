71c9f0350469f395e856f7b62c0e32a900da6dbe
TeooSwee <teoathle@gmail.com>
2026-02-19 12:03:20 +0100

    Test : inclusion de Accueil.php dans index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Test : affichage simple
-echo 'Index.php fonctionne sur Heroku';
+// Test : inclusion de Accueil.php
+echo '<b>Début index.php</b><br>';
+if (file_exists('Accueil.php')) {
+	include 'Accueil.php';
+	echo '<br><b>Fin index.php</b>';
+} else {
+	echo "<b>Erreur :</b> Accueil.php introuvable";
+}
 exit;
