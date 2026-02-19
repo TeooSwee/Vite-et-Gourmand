7bdc54958b43c2286a834734d4b4dc84afdc1e6c
TeooSwee <teoathle@gmail.com>
2026-02-19 13:37:36 +0100

    index.php : simple redirection vers Accueil.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-// Affiche les erreurs PHP pour le debug
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Test : inclusion de Accueil.php
-echo '<b>Début index.php</b><br>';
-if (file_exists('Accueil.php')) {
-	include 'Accueil.php';
-	echo '<br><b>Fin index.php</b>';
-} else {
-	echo "<b>Erreur :</b> Accueil.php introuvable";
-}
+header('Location: Accueil.php');
 exit;
